a2450eb02906a574bb78bc4a460793f296466a09
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2023-03-30 00:18:22 -0300

    Add date to datetime notif generator

--- genwindowsnotif.py
+++ genwindowsnotif.py
@@ -2,7 +2,7 @@ from win10toast import ToastNotifier
 from datetime import datetime
 
 toaster = ToastNotifier()
-toaster.show_toast("Time", datetime.now().strftime("%H:%M:%S"), duration=5)
+toaster.show_toast("Time", datetime.now().strftime("%d/%m/%y %H:%M:%S"), duration=5)
 
 while toaster.notification_active(): 
     time.sleep(0.1)
